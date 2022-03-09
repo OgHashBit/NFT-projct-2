ce714006c5ab2b78dc5c9806b2bb6cbcd71737a8
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-03-09 15:35:48 +0100

    Update HomeSection.js

--- src/pages/Home/HomeSection.js
+++ src/pages/Home/HomeSection.js
@@ -25,14 +25,12 @@ const HomeSection = () => {
             <Grid item xs={12} sm={12} md={12} lg={12}>
               <Typography className={classes.title}>What is ASTROHEDZ?</Typography>
               <Typography className={globalClasses.text21}>
-                Astrohedz is a collection of 10,000 unique NFTs living in the Enefty Galaxy (on the Ethereum
-                blockchain). What makes this project different than any other NFT project is that there are collections
-                within the colletion. Of the 10,000 aliens, there are four different species from 5 different planets
-                and the total possible combinations is greater than any previous generative NFT project to date. This
-                has been a colossal undertaking but we wanted to make something truly different than anything anyone has
-                seen before. Along with the collection of some of the most awesome NFTs (in the galaxy) we have designed
-                a play-to-earn action/adventure/crafting game with a focus on actually having fun and building a
-                commumity. Using input from the Astrohodlers, the future of this game and this galaxy is up to you.
+                Unique digital art collection made completely by Artificial intelligence backed by gold.
+                We adopted Paxos PAXG token for highly accessible, trusted, redeemable and flexible low fee backing.One fine troy ounce of gold equals one PAXG token (1:1). NFT owners can claim PAXG tokens depending on NFT rarity. Each rarity level has different amount of PAXG tokens available for claim, but also number of copy's available.
+                Common NFT - 0.5 $PAXG token
+                Rare NFT - 1 $PAXG token
+                Mystic NFT - 2 $PAXG token
+                Legendary NFT - 5 $PAXG token
               </Typography>
             </Grid>
           </Box>
